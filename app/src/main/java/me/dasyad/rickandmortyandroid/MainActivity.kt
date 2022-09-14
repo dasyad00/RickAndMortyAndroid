776001e31e0c634c4edff3cbd09ef9d81d17585d
Danang Rahmatullah <dasyad00@gmail.com>
2022-09-14 14:13:38 +0200

    Initial commit

---
+++ app/src/main/java/me/dasyad/rickandmortyandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.dasyad.rickandmortyandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
