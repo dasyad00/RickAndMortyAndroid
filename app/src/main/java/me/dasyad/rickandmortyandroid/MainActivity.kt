10e8ffd63a9cf136302e6d1cfcd80e2244555fbc
Danang Rahmatullah <dasyad00@gmail.com>
2022-09-14 16:28:29 +0200

    add recyclerview of character names

--- app/src/main/java/me/dasyad/rickandmortyandroid/MainActivity.kt
+++ app/src/main/java/me/dasyad/rickandmortyandroid/MainActivity.kt
@@ -1,15 +1,17 @@
 package me.dasyad.rickandmortyandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.launch
 import me.dasyad.rickandmortyandroid.databinding.ActivityMainBinding
 import me.dasyad.rickandmortyandroid.rest.CharacterApiClient
+import me.dasyad.rickandmortyandroid.ui.CharacterListAdapter
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -27,9 +29,14 @@ class MainActivity : AppCompatActivity() {
         val button = binding.startButton
         val textView = binding.textView
 
+        val adapter = CharacterListAdapter()
+        binding.characterList.layoutManager = LinearLayoutManager(this)
+        binding.characterList.adapter = adapter
+
         button.setOnClickListener {
             lifecycleScope.launch {
                 val characters = characterApiClient.getAllCharacters().results
+                adapter.setCharacters(characters)
                 Log.d("RickMortyAndroid", characters.toString())
             }
         }
---
+++ app/src/main/java/me/dasyad/rickandmortyandroid/ui/CharacterListAdapter.kt
@@ -0,0 +1,37 @@
+package me.dasyad.rickandmortyandroid.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import me.dasyad.rickandmortyandroid.databinding.CharacterViewBinding
+import me.dasyad.rickandmortyandroid.model.Character
+
+class CharacterListAdapter(
+
+) : RecyclerView.Adapter<CharacterViewHolder>() {
+    private var characters: List<Character> = emptyList()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = CharacterViewBinding.inflate(inflater, parent, false)
+        return CharacterViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
+        holder.bind(characters[position])
+    }
+
+    override fun getItemCount(): Int = characters.size
+
+    fun setCharacters(characters: List<Character>) {
+        this.characters = characters
+        this.notifyDataSetChanged()
+    }
+}
+
+class CharacterViewHolder(
+    private val binding: CharacterViewBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(character: Character) {
+        binding.name.text = character.name
+    }
+}
