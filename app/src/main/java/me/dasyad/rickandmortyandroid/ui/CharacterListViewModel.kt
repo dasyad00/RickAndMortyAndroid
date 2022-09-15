aa4564a7f2ac9325e63dd67aef7ba333dad18b52
Danang Rahmatullah <dasyad00@gmail.com>
2022-09-15 13:53:00 +0200

    wrap lists in mutexes

--- app/src/main/java/me/dasyad/rickandmortyandroid/ui/CharacterListViewModel.kt
+++ app/src/main/java/me/dasyad/rickandmortyandroid/ui/CharacterListViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.sync.Mutex
 import me.dasyad.rickandmortyandroid.model.Character
 import me.dasyad.rickandmortyandroid.rest.CharacterApiClient
 
@@ -14,9 +15,11 @@ class CharacterListViewModel(
     private var maxPages: Int = -1
     private var lastPageIndex = 1
 
+    private val mutexCharacters = Mutex()
     private val mCharacters: MutableLiveData<List<Character>> = MutableLiveData(listOf())
     val characters: LiveData<List<Character>> = mCharacters
 
+    private val mutexBitmaps = Mutex()
     private val mImageBitmaps: MutableLiveData<List<Bitmap>> = MutableLiveData((listOf()))
     val imageBitmaps = mImageBitmaps
 
@@ -33,19 +36,23 @@ class CharacterListViewModel(
             return
         }
 
+        mutexCharacters.lock()
         val list = this.characters.value!!.toMutableList()
         val nextList = characterApiClient.getAllCharacters(lastPageIndex).results
         list.addAll(nextList)
         mCharacters.value = list
+        lastPageIndex++
+        mutexCharacters.unlock()
 
+        mutexBitmaps.lock()
         val bitmaps = this.imageBitmaps.value!!.toMutableList()
         val newBitmaps = nextList.map { character ->
             this.getImageBitmap(character.image)
         }
         bitmaps.addAll(newBitmaps)
         mImageBitmaps.value = bitmaps
+        mutexBitmaps.unlock()
 
-        lastPageIndex++
     }
 
     private suspend fun getImageBitmap(url: String): Bitmap =
